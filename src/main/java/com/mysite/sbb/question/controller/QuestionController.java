5b772fbbca090456d3c012d5f70fe1e4ce555313
hyelim3 <shl0703ss@gmail.com>
2022-09-27 16:04:22 +0900

    질문 리스트 템플릿 작업

--- src/main/java/com/mysite/sbb/question/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestionController.java
@@ -1,12 +1,24 @@
 package com.mysite.sbb.question.controller;
 
+import com.mysite.sbb.question.dao.QuestionRepository;
+import com.mysite.sbb.question.domain.Question;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class QuestionController {
+
+    //데이터 조회하여 템플릿에 전달하기
+    private final QuestionRepository questionRepository; //null 주입을 할 때 final을 써줘야함, 의존성주입
     @RequestMapping("/question/list")
-    public String list() {
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
         return "question_list";
     }
 }
