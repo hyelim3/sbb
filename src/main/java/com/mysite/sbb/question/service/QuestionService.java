92e58558b4dd8165f68579c24e1c77f32afa3663
hyelim3 <shl0703ss@gmail.com>
2022-10-06 17:27:37 +0900

    일시 역순으로 조회 & 게시물 번호 질문 목록 템플릿에 ã 적용

--- src/main/java/com/mysite/sbb/question/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -7,9 +7,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -36,8 +38,10 @@ public class QuestionService {
     }
 
     public Page<Question> getList(int page){
+        List<Sort.Order> sorts = new ArrayList<>(); //Sort.Order에 객체로 구성된 리스트에 객체 추가
+        sorts.add(Sort.Order.desc("createDate")); //작성일시(createDate)를 역순(Desc)으로 조회
         //정수 타입의 페이지 번호를 입력받아 해당 페이지의 질문 목록을 리턴하는 메서드로 변경
-        Pageable pageable = PageRequest.of(page,10); // 한 페이지에 보여줄 게시글 10
+        Pageable pageable = PageRequest.of(page,10, Sort.by(sorts)); // 한 페이지에 보여줄 게시글 10, 소트 객체 생성
         return questionRepository.findAll(pageable);
     }
 
